0035fc7ac346b3ace58ecfe2687f4a4011ab4d4c
haitaolv <1247959126@qq.com>
2015-09-15 11:44:52 +0800

    modified

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int a = 1;
+        int b = 2;
+        int c = a + b;
+        System.out.println("c = "+c);
     }
 }
